0038dbe189eace7cf943456f7f102730619e3069
alexeisanko <alexeisanko@gmail.com>
2023-10-04 06:39:58 +0300

    added html subscription

--- chinglish/students/models.py
+++ chinglish/students/models.py
@@ -4,14 +4,14 @@ from django.utils.translation import gettext_lazy as _
 
 class Student(models.Model):
     first_name = models.CharField(_('first name'), max_length=50, null=True, default=None)
-    second_name = models.CharField(_('second name'), max_length=50, blank=True, null=True)
+    second_name = models.CharField(_('second name'), max_length=50, null=True, default=None)
     last_name = models.CharField(_('last name'), max_length=50, null=True, default=None)
-    phone = models.CharField(_('number phone'), max_length=15)
-    birthday = models.DateField(_('birthday'))
-    classroom = models.CharField(_('classroom'), max_length=12)
+    phone = models.CharField(_('number phone'), max_length=15, null=True, default=None)
+    birthday = models.DateField(_('birthday'), null=True, default=None)
+    classroom = models.CharField(_('classroom'), max_length=12, null=True, default=None)
     is_child = models.BooleanField(_('is this a child?'), default=False)
-    phone_parents = models.CharField(_('number phone parents'), max_length=15, null=True, blank=True)
-    photo = models.ImageField(_('photo student'), upload_to='student')
+    phone_parents = models.CharField(_('number phone parents'), max_length=15, null=True, default=None)
+    photo = models.ImageField(_('photo student'), upload_to='student', null=True, default=None)
     user = models.OneToOneField('users.User', on_delete=models.PROTECT, null=True)
 
     def get_full_name(self):
